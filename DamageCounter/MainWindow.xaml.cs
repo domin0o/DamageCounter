1f424408fc21f296954448d61ca3c23d3efa1873
Dominik Skrzydlewski <skrzydlewskijunior@gmail.com>
2023-02-12 23:36:45 +0100

    Dodaj pliki projektów.

---
+++ DamageCounter/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DamageCounter
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SwordDamage swordDamage = new SwordDamage();
+        Random random= new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage.SetMagic(magic.IsChecked.Value);
+            swordDamage.SetFlaming(flaming.IsChecked.Value);
+            RollDice();
+        }
+
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+        void DisplayDamage()
+        {
+            damage.Text = "Rzut: " + swordDamage.Roll + ", punkty obrażeń: " + swordDamage.Damage;
+        }
+
+        private void flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+
+        private void flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(false);
+            DisplayDamage();
+        }
+
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(true);
+            DisplayDamage();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
---
+++ DamageCounter/SwordDamage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DamageCounter
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FLAME_DAMAGE;
+            Debug.WriteLine($"Po wykonaniu CalculateDamage: {Damage} (rzut: {Roll})");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"Po wykonaniu SetMagic: {Damage} (rzut: {Roll})");
+        }
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if(isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+                Debug.WriteLine($"Po wykonaniu SetFlaming: {Damage} (rzut: {Roll})");
+            }
+        }
+    }
+}
